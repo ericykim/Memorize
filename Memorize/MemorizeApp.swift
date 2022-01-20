f0c6fbf1134aee772e6eae5f1f360176498f2f23
Eric Kim <ericykim5399@gmail.com>
2022-01-20 16:27:17 -0500

    init commit or memorize game

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Eric Kim on 1/18/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
